ecea5662a527e959252e18f1760d2d886f5d1932
Jérémy PAYET <jpayet@diginamic-formation.fr>
2022-06-01 18:05:27 +0400

    Exercice 1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,30 @@
+package ex1;
+
+/**
+ * Classe qui propose des opérations sur les nombres*
+ * 
+ * @author Jérémy Payet
+ */
+public class Calcul {
+	/**
+	 * Retourne l'addition de 2 entiers
+	 * 
+	 * @param a entier 1
+	 * @param b entier 2
+	 * @return int
+	 */
+	public int addition(int a, int b) {
+		return a + b;
+	}
+
+	/**
+	 * Retourne la soustraction de 2 entiers
+	 * 
+	 * @param a entier 1
+	 * @param b entier 2
+	 * @return int
+	 */
+	public int soustraction(int a, int b) {
+		return a - b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,55 @@
+package ex1;
+
+import java.util.Date;
+
+/**
+ * Classe qui définit une entreprise*
+ * 
+ * @author Jérémy Payet
+ */
+public class Entreprise {
+
+	private int siret;
+	private String nom;
+	private String adresse;
+	private Date dateCreation;
+
+	public static final int CAPITAL_MAX = 3000000;
+
+	public void Afficher_statut() {
+
+	}
+
+	public int getSiret() {
+		return siret;
+	}
+
+	public void setSiret(int siret) {
+		this.siret = siret;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+}
